37d3687a47f4e6797c9f9aedc27579d9b83de44b
Anderson Pozo <60895689+Anderson-Pozo@users.noreply.github.com>
2023-12-27 16:14:23 +0000

    this code is working fine

--- index.js
+++ index.js
@@ -2,23 +2,61 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require('dns');
+const { MongoClient } = require('mongodb');
+const urlParser = require('url');
 
 // Basic Configuration
+const client = new MongoClient(process.env.DB_URL);
+const db = client.db("test");
+const urls = db.collection("urls");
+
 const port = process.env.PORT || 3000;
 
-app.use(cors());
-
+app.use(cors({
+  origin: '*'
+}));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', function (req, res) {
+  console.log(req.body);
+  const url = req.body.url;
+  dns.lookup(urlParser.parse(url).hostname,
+    async (error, address) => {
+      if (!address) {
+        res.json({ error: 'Invalid URL' })
+      } else {
+        const urlCount = await urls.countDocuments({});
+        const urlDoc = {
+          url,
+          short_url: urlCount
+        };
+        const result = await urls.insertOne(urlDoc);
+        // console.log({ result });
+        res.json({ original_url: url, short_url: urlCount });
+      }
+    }
+  );
 });
 
-app.listen(port, function() {
+app.get('/api/shorturl/:short_url', async (req, res) => {
+  const short_url = req.params.short_url;
+
+  if (short_url) {
+    const urlDoc = await urls.findOne({ short_url: +short_url });
+    res.redirect(urlDoc.url);
+  }
+
+  return res.json({
+    error: "No short URL found for the given input"
+  });
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
